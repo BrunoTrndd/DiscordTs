c8a79c6cd22e4e1a7799d795dc8821e1148ce78d
BrunoTrndd <40573273+BrunoTrndd@users.noreply.github.com>
2021-07-27 12:23:44 -0300

    Delete src/Model/Config directory
    To make the repo public

--- src/Model/Config/config.ts
+++
@@ -1,8 +0,0 @@
-export enum Conf {
-    database = "rpg",
-    username = "postgres",
-    password = "bruno",
-    port     = 5432,
-    prefix   = "q",
-    apiKey   = "NzcxMTg2MzE2NDY0NzUwNjIz.X5odOQ.fFnMt8k74PSvtJjg-Cb0Vck-S1g"
-}
